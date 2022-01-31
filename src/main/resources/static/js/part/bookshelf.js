e69053ed9bb4962778c830bb4e3ec5db3de25d54
Hossori note <law.f1.vegas@gmail.com>
2022-01-31 10:07:56 +0900

    fix css and js.

--- src/main/resources/static/js/part/bookshelf.js
+++ src/main/resources/static/js/part/bookshelf.js
@@ -4,24 +4,27 @@ class Bookshelf {
 
     constructor(bookshelf, container) {
         let html = this.createHtml(bookshelf);
-        html.setAttribute('class', 'bookshelf');
         container.append(html);
     }
 
     createHtml(bookshelf) {
-        let div = document.createElement('div');
-
         let userName = document.createElement('p');
         userName.append(document.createTextNode(bookshelf.user.name))
         let name = document.createElement('p');
         name.append(document.createTextNode(bookshelf.name));
         let createdAt = document.createElement('p');
         createdAt.append(document.createTextNode(bookshelf.createdAt));
+        
+		let bookshelfInfoDiv = document.createElement('div');
+        bookshelfInfoDiv.append(userName);
+        bookshelfInfoDiv.append(name);
+        bookshelfInfoDiv.append(createdAt);
+        bookshelfInfoDiv.setAttribute('class', 'bookshelfInfo');
 
-        div.append(userName);
-        div.append(name);
-        div.append(createdAt);
+		let div = document.createElement('div');
+		div.append(bookshelfInfoDiv);
         div.setAttribute('id', 'bookshelf'+bookshelf.id);
+        div.setAttribute('class', 'bookshelf');
 
         return div;
     }
