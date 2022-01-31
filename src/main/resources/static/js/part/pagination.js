375f0e5e7c1b8c9537255443792d274c93441b13
Hossori note <law.f1.vegas@gmail.com>
2022-01-31 11:27:39 +0900

    create pagination.

--- src/main/resources/static/js/part/pagination.js
+++ src/main/resources/static/js/part/pagination.js
@@ -2,7 +2,8 @@
 
 class Pagination {
 
-    constructor(pages, container) {
+    constructor(pages, methodName, container) {
+		this.methodName = methodName;
         let div = this.createHtml(pages);
         container.append(div);
     }
@@ -13,9 +14,19 @@ class Pagination {
         let pageCount = pages.totalPages === 0 ? 1 : pages.totalPages;
         for(let i = 1; i <= pageCount; i++) {
             let li = document.createElement('li');
-            li.append(document.createTextNode(i));
+            let liChild;
+            if(i === pages.number+1) {
+				liChild = document.createTextNode(i);
+			} else {
+				liChild = document.createElement('a');
+				liChild.setAttribute('onclick', this.methodName+'('+(i-1)+')');
+				liChild.append(document.createTextNode(i));
+			}
+			li.append(liChild);
             ol.append(li);
         }
+        
+        ol.setAttribute('class', 'pagination');
 
         return ol;
     }
