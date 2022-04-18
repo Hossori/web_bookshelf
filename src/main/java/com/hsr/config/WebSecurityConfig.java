47e7cd672ad85d7bf45ebd3bd53388a670d64ca1
Hossori <law.f1.vegas@gmail.com>
2022-04-18 22:38:44 +0900

    in the middle of creating login.

--- src/main/java/com/hsr/config/WebSecurityConfig.java
+++ src/main/java/com/hsr/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.hsr.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity webSecurity) throws Exception {
         webSecurity.ignoring().antMatchers(
             "/css/**",
-            "/images/**",
+            "/image/**",
             "/js/**",
             "/lib/**"
         );
@@ -29,7 +30,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .usernameParameter("name")
+                .passwordParameter("password")
+                .successForwardUrl("/bookshelf/index?page=0")
+                .failureUrl("/login?error")
+                .permitAll()
+            .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder authMgBuilder) throws Exception {
+        authMgBuilder
+            .inMemoryAuthentication()
+                .withUser("user")
+                .password("pass")
+                .roles("USER");
     }
 
 }
