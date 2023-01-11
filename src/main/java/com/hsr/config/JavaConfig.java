a9baecb50a8a00cbe66fc0476240bcc592b39490
Hossori <law.f1.vegas@gmail.com>
2023-01-11 20:45:37 +0900

    add Japanese validation message.(but property file is commented out because bug appears in login page.

--- src/main/java/com/hsr/config/JavaConfig.java
+++ src/main/java/com/hsr/config/JavaConfig.java
@@ -6,18 +6,32 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class JavaConfig {
+public class JavaConfig implements WebMvcConfigurer {
+
+    @Override
+    public Validator getValidator() {
+        return validator();
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(messageSource());
+        return validator;
+    }
+
     @Bean
     public MessageSource messageSource() {
-
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasenames("i18n/messages", "i18n/validationMessages", "i18n/attributes");
         messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
         messageSource.setFallbackToSystemLocale(false); // この設定のために作成した
 
         return messageSource;
-
     }
 }
